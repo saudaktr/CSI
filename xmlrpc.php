1ba62880662b15fb0cffde21b344d53a21676fe1
Saud Akhtar Siddiqui <saud.aktr@yahoo.com>
2015-07-11 23:50:25 +0530

    All files upload

---
+++ xmlrpc.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://school.wpshow.me</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://school.wpshow.me/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://school.wpshow.me/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://school.wpshow.me/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://school.wpshow.me/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
